9ec5ef61fb21e61a9ae8cbb4594f5872172b7434
Baidya Sarkar <bns@outlook.in>
2024-11-10 23:27:41 -0500

    Mission go global

---
+++ scripts/connect.js
@@ -0,0 +1,5 @@
+function redirectToPage(event) {
+    event.preventDefault(); // Prevents the form from submitting
+    window.location.href = "connect.html"; // Replace with the target page URL
+}
+
