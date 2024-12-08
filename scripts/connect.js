b762c80048a772f07d61d7de097eae2ddc5b870b
Baidya Sarkar <bns@outlook.in>
2024-12-08 15:33:46 -0500

    Mission go online

--- scripts/connect.js
+++ scripts/connect.js
@@ -3,3 +3,19 @@ function redirectToPage(event) {
     window.location.href = "connect.html"; // Replace with the target page URL
 }
 
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent default form submission
+  const form = event.target;
+
+  // Submit the form data to Google Form
+  fetch(form.action, {
+	method: 'POST',
+	body: new FormData(form),
+	mode: 'no-cors' // Required for cross-origin requests
+  }).then(() => {
+	// Redirect to Thank You page
+	window.location.href = 'connect.html';
+  }).catch(() => {
+	alert('Error submitting your query. Please try again.');
+  });
+}
