d7cd5b92832af0d69f2cbf9280a2a52b3c14d668
Baidya Sarkar <bns@outlook.in>
2025-01-07 15:21:32 -0500

    Mission go online

--- scripts/art-filter.js
+++ scripts/art-filter.js
@@ -6,16 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
         let selectedTag = "all"; // Initially, all images are shown
         tag.addEventListener("click", function () {
             const currentTag = this.getAttribute("data-tag");
-
-            // Remove active tag immediately
-            tags.forEach(t => t.classList.remove("active"));
-
+			
             // If the same tag is clicked again, reset to show all images
             if (selectedTag === currentTag) {
                 selectedTag = "all";
+                tags.forEach(t => t.classList.remove("active"));
             } else {
                 selectedTag = currentTag;
-                // Update active tag style
+                tags.forEach(t => t.classList.remove("active"));
                 this.classList.add("active");
             }
 
